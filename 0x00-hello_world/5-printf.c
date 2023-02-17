457cba3733ece78f35eff36e8df02e4b2e0fe99c
foreleanmore <foreleanmore@gmail.com>
2023-02-17 09:04:36 -0800

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include	<stdio.h>
+
+/**
+ * main	-	A program that print a line using printf function
+ *
+ * Return:	Always	0	(success)
+ */
+
+int	main(void)
+
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return	(0);
+}
+
