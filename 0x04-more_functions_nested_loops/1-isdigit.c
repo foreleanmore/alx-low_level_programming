add08152a24259b05390aa5e3c27ba733dad7c08
foreleanmore <foreleanmore@gmail.com>
2023-02-24 00:23:30 -0800

    isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include	"main.h"
+
+/**
+ * _isdigit	-	checks if numbers are 0 through 9
+ * @c:	char to be checked
+ *
+ * Return:	1 or 0
+ */
+
+int	_isdigit(int c)
+{
+	if	(c >= '0' && c <= '9')
+		return	(1);
+	else
+		return	(0);
+	_putchar('\n');
+}
+
