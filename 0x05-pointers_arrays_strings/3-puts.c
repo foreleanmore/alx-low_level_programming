f08dea65b63da24d57637b2d5696061c4de2b244
foreleanmore <foreleanmore@gmail.com>
2023-02-27 06:15:26 -0800

    I do not fear computers. I fear the lack of them

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include	"main.h"
+
+/**
+ * _puts	-	prints a string followed by new line
+ * @str:	string to be printed
+ */
+
+void	_puts(char *str)
+{
+	while	(*str != '\n')
+	{
+		_putchar(*str++);
+	}
+	_putchar('\n');
+}
+
