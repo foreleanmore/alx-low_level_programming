b6ed1226cb8697a5d3a0a948dc12983018930717
foreleanmore <foreleanmore@gmail.com>
2023-02-28 04:02:48 -0800

    strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -2,22 +2,25 @@
 
 /**
  * _strcpy	-	copies the string pointed to by src
- * @dest:	destinations
- * @src:	source
+ * @dest:	copy to
+ * @src:	copy from
  * Return:	string
  */
 
 char	*_strcpy(char *dest, char *src)
 {
 	int	len	=	0;
+	int	x	=	0;
 
 	while	(*(src + len) != '\0')
 	{
-		*(dest + len)	=	*(str + len);
 		len++;
 	}
-
-	*(dest + len)	=	'\0';
+	for	( ; x < 1; x++)
+	{
+		dest[x]	=	src[x];
+	}
+	dest[len]	=	'\0';
 	return	(dest);
 }
 
