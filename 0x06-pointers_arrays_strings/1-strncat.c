64db823fcbe12637e48aac296c63d14b6b80a636
foreleanmore <foreleanmore@gmail.com>
2023-03-02 23:46:31 -0800

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,19 +12,21 @@
 char	*_strncat(char *dest, char *src, int n)
 {
 	int	j;
+	int	i;
 
+	i	=	0;
+	while	(dest[i] != '\0')
+	{
+		i++;
+	}
 	j	=	0;
 	while	(j < n && src[j] != '\0')
 	{
 		dest[j]	=	src[j];
+		i++;
 		j++;
 	}
-	while	(j < n)
-	{
-		dest[j]	=	'\0';
-		j++;
-	}
-
+	dest[i]	=	'\0';
 	return	(dest);
 }
 
